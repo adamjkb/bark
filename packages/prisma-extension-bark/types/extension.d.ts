c103c7350a4982569d7e09a820d47455b2d9bf4a
Adam Jakab <adam.jakab@bureau-va.com>
2023-07-24 22:44:49 +0200

    type clean up

--- packages/prisma-extension-bark/types/extension.d.ts
+++ packages/prisma-extension-bark/types/extension.d.ts
@@ -1,16 +1,11 @@
 import { BarkMethods } from ".";
-import type { Prisma } from '@prisma/client'
-import { Types } from '@prisma/client/runtime/library'
-
-type BasePrismaMethodKeys =  Exclude<Types.Public.Operation, `$${string}`>
 
 /**
- * TODO: Create extension context
  * Bark extension context
  * The builtin functions are specific to the test environment (SQLite)
  * which is technically the smallest subset of available functions
  * the `getExtensionContext` method exposes other ones too + "fields"
- * @deprecated TOOD
+ * @deprecated
  */
 export type BarkExtensionContext<T> = BarkMethods & { name?: string | undefined; }
 
